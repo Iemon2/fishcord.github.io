9cb2d06464b7c589062289230cdd00eb10f8d8ea
dreams-of-somnus <lemonwithoutthehead@gmail.com>
2026-03-01 19:35:18 -0600

    added the breakable objects to kibty game (no functionality)

--- Games/kibtygame/meow.js
+++ Games/kibtygame/meow.js
@@ -18,6 +18,8 @@ window.onload = function() {
     });
 }
 
+window.assetFolder = "../../Assets/default-theme/kibtygame/" 
+
 sprite = {
     x: 0,
     y: 0,
@@ -30,24 +32,52 @@ function initializeState() {
     //drawingsurface
 
     function State(imagesrc) {
+
+        this.canvas = document.querySelector("canvas");
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.drawingSurface = this.canvas.getContext("2d");
+
+        this.breakables = getBreakableObjects(7);
         this.kibty = Object.create(sprite);
         this.kibty.image = new Image();
         this.kibty.image.src = imagesrc;
         this.kibty.image.getState = function getState() {
             return this;
         }
-
-        this.canvas = document.querySelector("canvas");
-        this.drawingSurface = this.canvas.getContext("2d");
         
+        //console.log(this.breakables[1]);
         //console.log(this.kibty);
         //console.log(this.canvas);
         //console.log(this.drawingSurface);
     }
 
-    const state = new State("../../Assets/default-theme/Kitty.png");
+    const state = new State(assetFolder + "Kitty.png");
 
     return state;
+}
+
+function getBreakableObjects(amount) {
+    let distanceMultiplierX = .08 * document.getElementById("game-container").width;
+    let distanceMultiplierY = .1 * document.getElementById("game-container").height;
+
+    let breakables = [];
+    for (let i = 0; i <= amount; i++) {
+        let breakableItem = {
+            image: new Image(),
+            broken: false,
+            x: 0,
+            y: 0,
+            width: 600,
+            height: 240
+        }
+        breakableItem.x = (Math.floor(Math.random() * 10)) * distanceMultiplierX;
+        breakableItem.y = (Math.floor(Math.random() * 8)) * distanceMultiplierY;
+        breakableItem.image.src = assetFolder + "Vase.png";
+        breakables.push(breakableItem);
+        console.log(breakableItem.x + ", " + breakableItem.y)
+    } 
+    return breakables;
 }
 
 function handleKeydownEvent(kibty, e) {
@@ -59,22 +89,22 @@ function handleKeydownEvent(kibty, e) {
         isMovement = false;
         switch (e.key) {
             case "w":
-                console.log("w");
+                //console.log("w");
                 moveKibty(kibtyRef, 0, -1);
                 isMovement = true;
                 break;
             case "s":
-                console.log("s");
+                //console.log("s");
                 moveKibty(kibtyRef, 0, 1);
                 isMovement = true;
                 break;
             case "a":
-                console.log("a");
+                //console.log("a");
                 moveKibty(kibtyRef, -1, 0);
                 isMovement = true;
                 break;
             case "d":
-                console.log("d");
+                //console.log("d");
                 moveKibty(kibtyRef, 1, 0);
                 isMovement = true;
                 break;
@@ -101,22 +131,71 @@ function render(drawingSurface) {
     if (typeof stateRef !== 'undefined') {
         stateRef.drawingSurface.clearRect(0,0, stateRef.canvas.width, stateRef.canvas.height);
 
-        stateRef.drawingSurface.drawImage(
-            stateRef.kibty.image,
-            stateRef.kibty.x,
-            stateRef.kibty.y,
-            60,
-            90
-        );
+        sceneObjects = getObjectLocations(stateRef);
+        renderBehind(stateRef, sceneObjects[1]);
+        renderKibty(stateRef);
+        renderFront(stateRef, sceneObjects[0]);
+    }
+}
+
+function getObjectLocations(state) {
+    breakables = state.breakables;
+    objectsBehind = [];
+    objectsFront = [];
+    for (i = 0; i < breakables.length; i++) {
+        if ((breakables[i].y + (breakables[i].height/4)) > (state.kibty.y)) {
+            objectsBehind.push(breakables[i]);
+        }
+        else {
+            objectsFront.push(breakables[i]);
+        }
+    }
+    breakables = [objectsBehind, objectsFront];
+    return breakables;
+}
+
+
+function renderBehind(state, objects) {
+    for (i = 0; i < objects.length; i++) {
+        state.drawingSurface.drawImage(
+        objects[i].image,
+        objects[i].x,
+        objects[i].y,
+        objects[i].width,
+        objects[i].height
+    );
+    }
+}
+
+function renderKibty(state) {
+    state.drawingSurface.drawImage(
+        state.kibty.image,
+        state.kibty.x,
+        state.kibty.y,
+        120,
+        120
+    );
+}
+
+function renderFront(state, objects) {
+    for (i = 0; i < objects.length; i++) {
+        state.drawingSurface.drawImage(
+        objects[i].image,
+        objects[i].x,
+        objects[i].y,
+        objects[i].width,
+        objects[i].height
+    );
     }
 }
 
 function moveKibty(kibty, x, y) {
-    movementMultiplier = 10;
+    movementMultiplierX = 20;
+    movementMultiplierY = 10;
 
-    kibty.y = kibty.y + (y*movementMultiplier);
-    kibty.x = kibty.x + (x*movementMultiplier);
+    kibty.y = kibty.y + (y*movementMultiplierY);
+    kibty.x = kibty.x + (x*movementMultiplierX);
 
-    console.log(kibty.x)
-    console.log(kibty.y)
+    //console.log(kibty.x)
+    //console.log(kibty.y)
 }
